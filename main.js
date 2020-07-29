7a8d77f62f12c672fca1520f0484bfe092788c5d
arisa83 <arisa19920803@gmail.com>
2020-07-29 09:06:08 +0900

    課題修正

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-let answer = "東京";
-
-while(answer == "東京"){
+while (true) {
     answer = prompt("日本の首都は？");
-if(answer == "東京"){
-    alert("正解です！");
-}else{
-    alert("不正解です！");
+    if (answer === "東京") {
+        alert("正解です！");
+    } else {
+        alert("不正解です！");
     }
 }
